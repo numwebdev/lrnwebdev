1512a8f34f36946627e72b95141dc3ff289f59d2
mdnmnahmed <mdnmnahmed@gmail.com>
2020-07-05 12:51:02 +0530

    Updates

---
+++ precache-manifest.e9cca9e39381ef5ec84dc1d6d90a289d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b3ba34e0ec08cb841a6da4d7fb41a2",
+    "url": "/lrnwebdev/index.html"
+  },
+  {
+    "revision": "336d34a8d0fb80d6e028",
+    "url": "/lrnwebdev/static/js/2.bd741346.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lrnwebdev/static/js/2.bd741346.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "146f9e3cc6986111f0ba",
+    "url": "/lrnwebdev/static/js/main.749911ee.chunk.js"
+  },
+  {
+    "revision": "cf9a3c10b689f900a8a3",
+    "url": "/lrnwebdev/static/js/runtime-main.78fe1839.js"
+  }
+]);
